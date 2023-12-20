fe5132b28d58b9228d0189775ff1364adb51cb71
Kleyton Caires <kleytoncaires@gmail.com>
2023-12-20 10:22:10 -0300

    feat: svg support

--- functions/content.php
+++ functions/content.php
@@ -65,3 +65,37 @@ add_filter('excerpt_length', 'custom_excerpt_length', 999);
 //     return '...';
 // }
 // add_filter('excerpt_more', 'custom_more_excerpt');
+
+
+// -----------
+// SVG SUPPORT
+// -----------
+function allow_svg_upload($mimes)
+{
+	$mimes['svg'] = 'image/svg+xml';
+	return $mimes;
+}
+add_filter('upload_mimes', 'allow_svg_upload');
+
+function svg_thumbnail_support()
+{
+	if (function_exists('add_theme_support')) {
+		add_theme_support('post-thumbnails');
+		add_filter('wp_generate_attachment_metadata', 'svg_attachment_metadata', 10, 2);
+	}
+}
+
+function svg_attachment_metadata($metadata, $attachment_id)
+{
+	$attachment = get_post($attachment_id);
+	$mime_type  = get_post_mime_type($attachment);
+
+	if ('image/svg+xml' === $mime_type) {
+		$metadata['width']  = 0;
+		$metadata['height'] = 0;
+	}
+
+	return $metadata;
+}
+
+add_action('after_setup_theme', 'svg_thumbnail_support');
